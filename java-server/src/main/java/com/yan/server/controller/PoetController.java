425e7f7f008a3e2d8f2fefdee04a9c806c924524
yan160100 <1909484942@qq.com>
2023-03-03 10:08:35 +0800

    使用map接收参数

--- java-server/src/main/java/com/yan/server/controller/PoetController.java
+++ java-server/src/main/java/com/yan/server/controller/PoetController.java
@@ -1,43 +1,42 @@
 package com.yan.server.controller;
 
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.TypeReference;
+import org.apache.commons.io.FileUtils;
+import org.springframework.web.bind.annotation.*;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.HashMap;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
-import lombok.Data;
-import org.apache.commons.io.FileUtils;
 /**
  * @author yan
  **/
 @RestController
 public class PoetController {
-    @GetMapping("/poet")
-    public Map<String, Object> list() throws IOException {
-        Map<String, Object> map = new HashMap<>();
+    @GetMapping ("/poet")
+    public List<Map<String, Object>> list(@RequestBody Map<String, Object> map) {
         String separator = File.separator;
-        ClassPathResource classPathResource = new ClassPathResource("json" + separator + "poet300.json");
-        File file = classPathResource.getFile();
+        String path = System.getProperty("user.dir") + separator + "src" + separator + "main" + separator +
+                "resources" + separator + "json" + separator + "poet300.json";
 
-        //将file对象转为字符串
-        String s = FileUtils.readFileToString(file);
-        //将字符串解析为JSONObject对象
-        JSONObject jsonObject = JSONObject.parseObject(s);
-        //获取数组对象
-        JSONArray sites = jsonObject.getJSONArray("sites");
-        //遍历数组对象
-        sites.forEach(x -> {
-            System.out.println(x.toString());
+        File file = new File(path);
+        String str = null;
+        try {
+            str = FileUtils.readFileToString(file, "UTF-8");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        List<Map<String, Object>> list1 = JSON.parseObject(str, new TypeReference<List<Map<String, Object>>>() {
         });
-        //将json转为对象
-        map = JSONObject.parseObject(s, HashMap.class);
-        System.out.println(map);
-        return map;
+        List<Map<String, Object>> list2 = new ArrayList<Map<String, Object>>();
+        for (int i = 0; i < list1.size(); ++i) {
+            if (list1.get(i).get("author").toString().equals(map.get("author").toString())) {
+                list2.add(list1.get(i));
+            }
+        }
+        return list2;
     }
+
 }
