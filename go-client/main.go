28f4302cb56e09ad134a8246311f6e80fc0cc4d6
yan160100 <1909484942@qq.com>
2023-03-03 17:01:11 +0800

    添加go-client

---
+++ go-client/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func main() {
+	m := make(map[string]interface{})
+	m["author"] = "李白"
+	str, _ := json.Marshal(m)
+	reader := bytes.NewReader(str)
+
+	request, _ := http.NewRequest("GET", "http://localhost:8083/poet", reader)
+	request.Header.Set("Content-Type", "application/json;charset=utf-8")
+	resp, _ := http.DefaultClient.Do(request)
+	defer resp.Body.Close()
+
+	bt, _ := io.ReadAll(resp.Body)
+
+	var m1 []map[string]interface{}
+	_ = json.Unmarshal(bt, &m1)
+	fmt.Println(m1)
+}
