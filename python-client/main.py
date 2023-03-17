1960b5c183ef2cee8b25a604e84b928c6abe4a31
yan160100 <1909484942@qq.com>
2023-03-17 14:13:02 +0800

    改名

--- python-client/main.py
+++ python-client/main.py
@@ -3,6 +3,6 @@ import requests
 obj = {
     "ci": "宸仪"
 }
-res = requests.get('http://127.0.0.1:8084/explanation', params=obj)
+res = requests.get('http://127.0.0.1:8084/explain', params=obj)
 
-print(res.text)
+
