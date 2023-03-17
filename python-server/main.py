67047ef5fe9ac0dc783385098a8eed17d7a4b0a3
yan160100 <1909484942@qq.com>
2023-03-17 13:58:03 +0800

    获取请求头和请求体

--- python-server/main.py
+++ python-server/main.py
@@ -1,12 +1,12 @@
 import json
 from fastapi import FastAPI, Request
 import uvicorn
+from typing import Dict
 
 f = open('./json/ci.json', encoding='utf-8')
 d = json.load(f)
 
 app = FastAPI()
-
 
 @app.get("/explanation")
 async def explanation(request: Request):
@@ -15,6 +15,13 @@ async def explanation(request: Request):
         if i['ci'] == ci:
             return i['explanation']
 
+
+@app.post("/explanation")
+async def explanation(request: Request, dict: Dict):
+    obj = {}
+    obj['Headers'] = request.headers
+    obj['body'] = dict
+    return obj
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", port=8084, reload=True)
